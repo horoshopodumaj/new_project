4426c5269236b1de5f5f9856ef3092357110eab0
Diana Murtazina <murka8831@yandex.ru>
2024-05-09 23:59:25 +0300

    fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
+import { useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "helpers/classNames/classNames";
+import { AboutPageAsync } from "pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "pages/MainPage/MainPage.async";
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { classNames } from "./helpers/classNames/classNames";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import "./styles/index.scss";
-import { useTheme } from "./theme/useTheme";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
