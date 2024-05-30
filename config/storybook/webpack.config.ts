5da6b46c3a0f506a6b008bb15e455d39caeafeac
Diana Murtazina <murka8831@yandex.ru>
2024-05-30 18:53:35 +0300

    stories for sidebar

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
@@ -13,6 +13,24 @@ export default ({ config }: {config: webpack.Configuration}) => {
 
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
+
+    if (config.module?.rules) {
+        // eslint-disable-next-line no-param-reassign
+        config.module.rules = config.module?.rules?.map(
+            (rule: RuleSetRule | null | undefined | false | 0 | '' | '...') => {
+                if (rule && rule !== '...' && /svg/.test(rule.test as string)) {
+                    return { ...rule, exclude: /\.svg$/i };
+                }
+
+                return rule;
+            },
+        );
+    }
+
+    config.module?.rules?.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
 
     config.module?.rules?.push(buildCssLoader(true));
 
