ef0575f03e9babfff2340a328b15a539c2c29d4e
Diana Murtazina <murka8831@yandex.ru>
2024-06-04 22:59:02 +0300

    rework sidebar button

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -4,23 +4,31 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
-    OUTLINE='outline'
+    OUTLINE='outline',
+    BACKGROUND='background',
+    BACKGROUND_INVERTED='backgroundInverted',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ThemeButton;
+    square?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
     const {
-        className, children, theme, ...otherProps
+        className, children, theme, square, ...otherProps
     } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls[theme]]: true,
+        [cls.square]: square,
+    };
 
     return (
         <button
             type="button"
-            className={classNames(cls.button, {}, [className, cls[theme]])}
+            className={classNames(cls.button, mods, [className, cls[theme]])}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...otherProps}
         >
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button';
+import { ThemeButton } from 'shared/ui/Button/ui/Button';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
@@ -29,8 +30,15 @@ export const Sidebar: React.FC<SidebarProps> = (props) => {
                 [className],
             )}
         >
-            <Button data-testid="sidebar-toggle" type="button" onClick={onToggle}>
-                {t('Нажми')}
+            <Button
+                data-testid="sidebar-toggle"
+                type="button"
+                onClick={onToggle}
+                className={cls.collapsedBtn}
+                theme={ThemeButton.BACKGROUND_INVERTED}
+                square
+            >
+                {collapsed ? '>' : '<'}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
